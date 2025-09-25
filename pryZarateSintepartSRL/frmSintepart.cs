fc8c85ba1d5d5c27a85628cb2bafd064bd65d5f0
Florencia Zarate <florenciazarate2110@gmail.com>
2025-09-24 22:57:09 -0300

    Agregar archivos de proyecto.

---
+++ pryZarateSintepartSRL/frmSintepart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryZarateSintepartSRL
+{
+    public partial class frmSintepart : Form
+    {
+        public frmSintepart()
+        {
+            InitializeComponent();
+        }
+    }
+}
